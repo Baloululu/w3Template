71fbec4bd351044a60976402f5879a87d87cc9c6
Baloululu <bluchier@laposte.net>
2019-07-27 19:06:39 +0200

    Initial template commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+
+        <title>Template</title>
+    </head>
+    <body>
+    <div class="w3-content">
+        <div class="w3-container w3-center">
+            <h1>Test w3School.CSS</h1>
+        </div>
+    </div>
+
+    </body>
+</html>
